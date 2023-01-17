488bf0193da43d8eb4fb593e644c2e3b8262298e
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-01-17 12:06:41 -0300

    handlebars

---
+++ postDeal/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+const formRoutes = require('./routes/formRoute')
+
+const basePath = path.join(__dirname, '../templates')
+
+//ler o body
+app.use(express.urlencoded({
+    extended: true, 
+}))
+
+app.use(express.json())
+
+//*************/
+
+app.use(express.static('public'))
+
+app.use('/users', formRoutes)
+
+app.get('/', (req, res)=>{
+    res.sendFile(`${basePath}/index.html`)
+})
+
+
+app.listen(port, () =>{
+    console.log(`App rodando na porta ${port}`)
+
+})
+
